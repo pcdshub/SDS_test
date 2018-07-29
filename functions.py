015af6047fc04f3428e1921e2b3bcff9151e5228
pawanon61 <pawanon61@gmail.com>
2018-07-29 12:55:32 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -283,7 +283,7 @@ def post_to_confluence(space, parent_title, child_title, html_string, images):
 			with open(image, 'rb') as f:
 				data = f.read()
 			server.confluence2.addAttachment(token, child_page['id'], attachment, xmlrpclib.Binary(data));	
-		("visit the link to view the child page \n https://confluence.slac.stanford.edu/display/PCDS/sample+delivery+system+auto+test+results")
+		print ("visit the link to view the child page \n https://confluence.slac.stanford.edu/display/PCDS/sample+delivery+system+auto+test+results")
 		
 	except xmlrpclib.Fault as err: 
 	   print ("Error accessing Confluence:", sys.exc_info()[0], err.faultString)
